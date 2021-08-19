6e08668731075b6410d99fb28f151c1ada49a6eb
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-08-19 06:23:47 -0700

    Styled left-side of nav bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,24 @@
 import './topbar.scss';
+import { Person, Mail } from '@material-ui/icons';
 
 export default function Topbar() {
   return (
     <div className='topbar'>
       <div className='wrapper'>
         <div className='left'>
-          <a href='#intro'>Logo</a>
+          <a href='#intro' className='logo'>
+            francis
+          </a>
+          <div className='itemContainer'>
+            <Person className='icon' />
+            <span> (510) 921-9892</span>
+          </div>
+          <div className='itemContainer'>
+            <Mail className='icon' />
+            <span> tflesui@gmail.com</span>
+          </div>
         </div>
-        <div className='right'></div>
+        <div className='right'>Right Side</div>
       </div>
     </div>
   );
