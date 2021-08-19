e4a1743b807f79b83423e7096c658a128bc3a505
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-08-19 07:03:44 -0700

    Added animated menu hamburger

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,16 @@
 import './topbar.scss';
-import { Person, Mail } from '@material-ui/icons';
+import { Smartphone, Mail } from '@material-ui/icons';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className='topbar'>
+    <div className={'topbar ' + (menuOpen && 'active')}>
       <div className='wrapper'>
         <div className='left'>
           <a href='#intro' className='logo'>
             francis
           </a>
           <div className='itemContainer'>
-            <Person className='icon' />
+            <Smartphone className='icon' />
             <span> (510) 921-9892</span>
           </div>
           <div className='itemContainer'>
@@ -18,7 +18,13 @@ export default function Topbar() {
             <span> tflesui@gmail.com</span>
           </div>
         </div>
-        <div className='right'>Right Side</div>
+        <div className='right'>
+          <div className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+          </div>
+        </div>
       </div>
     </div>
   );
