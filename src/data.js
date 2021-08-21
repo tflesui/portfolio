b66011e8859897ce93977f829e36ebb4ca184f16
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-08-21 16:22:10 -0700

    Completed Portfolio section

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: 'Ecommerce Site',
+    img: 'assets/BFA Thumbnail.png'
+  },
+  // {
+  //   id: 2,
+  //   title: 'Ecommerce Site',
+  //   img: 'assets/BFA Thumbnail.png'
+  // },
+  // {
+  //   id: 3,
+  //   title: 'Ecommerce Site',
+  //   img: 'assets/BFA Thumbnail.png'
+  // }
+]
+
+export const webPortfolio = [
+  {
+    id: 1,
+    title: 'Ecommerce Site',
+    img: 'assets/BFA Thumbnail.png'
+  },
+  {
+    id: 2,
+    title: 'Ecommerce Site',
+    img: 'assets/BFA Thumbnail.png'
+  },
+  {
+    id: 3,
+    title: 'Ecommerce Site',
+    img: 'assets/BFA Thumbnail.png'
+  }
+]
